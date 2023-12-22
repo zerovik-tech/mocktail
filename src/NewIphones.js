7d4cde4cb57887ca1c7e2082e12e0f3148f225de
Sachin Pandey <pandeysachin9988@gmail.com>
2023-12-22 11:53:22 +0530

    added process and processing in the button, corrected the sequence of operation using promises,disabled input while processing and downloading

--- src/NewIphones.js
+++ src/NewIphones.js
@@ -1,5 +1,5 @@
 
-import React, { useState,useEffect} from 'react';
+import React, { useState,useEffect, useRef} from 'react';
 import mockup14ProImage from './images/iphone14ProMockup/mockup14Pro.png';
 import mockup14Image from './images/iphone14Mockup/mockup14.png';
 import './index.css'
@@ -9,14 +9,18 @@ import { InputLabel } from '@material-ui/core';
 import {Button,Checkbox, FormControlLabel } from '@mui/material';
 import {useNavigate } from 'react-router-dom';
 
-function ImageCombiner() {
+
+function NewIphonesImageCombiner() {
     const navigate = useNavigate();
+    const fileInputRef = useRef(null);
+    
   const [checked1, setChecked1] = useState(true);
   const [checked2, setChecked2] = useState(true);
-  const [inputArray,setInputArray] = useState([]);  
-  const [downloadButton, setDownloadButton] = useState(false)
-
-  const [isUploading, setIsUploading] = useState(false);
+  const [inputArray,setInputArray] = useState([]);  // stores initial input
+  const [downloadButton, setDownloadButton] = useState(false) // responsible for showing downloading or download
+  
+  const [isUploading, setIsUploading] = useState(false); // responsible for showing number of selected items
+  const [processImage, setProcessImage] = useState(false)
 
 
   const [images1, setImages1] = useState([]); //for mockup14Pro stores resized images
@@ -131,42 +135,75 @@ function ImageCombiner() {
 
 
   const handleImageChange = (e) => {
-    setIsUploading(true);
-    setImages1([])
-    setImages2([])
-    const selectedFiles = e.target.files; // Get the list of selected files
+    // setInputArray([])
+    
+    // setImages1([])
+    // setImages2([])
+    // setProcessImage(false)
+    // setDownloadButton(false)
+
+    // const selectedFiles = e.target.files; // Get the list of selected files
+    // if (fileInputRef.current) {
+    //     fileInputRef.current.value = '';
+    //   }
+    // console.log("selectedFiles")
+    // console.log(selectedFiles);
+    // console.log(selectedFiles.length)
+
+    // const fileArray = Array.from(selectedFiles);
+    // setInputArray(fileArray);
+    // setIsUploading(true);
+
+    setInputArray((prev) => { 
+        return []
+    });
+
+    setImages1((prev) => { 
+        return []
+    });
+
+    setImages2((prev) => { 
+        return []
+    });
+
+    setProcessImage((prev) => { 
+        return false
+    });
+
+    setDownloadButton((prev) => { 
+        return false
+    });
+
+    const selectedFiles = e.target.files;
+    
+
     console.log("selectedFiles")
     console.log(selectedFiles);
     console.log(selectedFiles.length)
 
     const fileArray = Array.from(selectedFiles);
-    setInputArray(fileArray);
+    if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
 
-   };
+    setInputArray((prev) => { 
+        return fileArray
+    });
+
+    
+
+    setIsUploading((prev) => { 
+        return true
+    });
+
+};
  
-  useEffect (() => { 
-    console.log('printing inputArray in useEffect Hook for inputArray : ', inputArray)
+//   useEffect (() => { 
+//     console.log('printing inputArray in useEffect Hook for inputArray : ', inputArray)
    
-    for(let i = 0 ; i < inputArray.length ; i++){ 
-      const file = inputArray[i];
-    const reader = new FileReader();
+    
 
-    reader.onload = (event) => {
-      const img = new Image();
-      img.src = event.target.result;
-      img.onload = () => {
-        // Resize image1
-        resizeImage(img,setImages1,1180,2556);
-        resizeImage(img,setImages2,2339,5063);
-        
-      };
-    };
-
-    reader.readAsDataURL(file);
-
-    }
-
-  },[inputArray])
+//   },[inputArray])
 
 
 
@@ -174,59 +211,127 @@ function ImageCombiner() {
 
 
   const handleClick = () => {
+
     if(inputArray.length === 0){ 
         alert('Please select a few items first')
         return
     }
-    setIsUploading(false);
+
+    
 
     if (!checked1 && ! checked2){ 
       alert("Please check the boxes for iphone");
     } else { 
 
-        setDownloadButton(true)
-
-      if (checked1 && checked2) { 
-        for (let i = 0; i < images1.length; i++) {
-      
-          combineImages(images1[i], mockup14Pro, `mockup14Pro-${i}`, 80, 80);
-          combineImages(images2[i], mockup14, `mockup14-${i}`, 348, 320);
-          
-        }
-
-      } else if (checked1) { 
         
-        for (let i = 0; i < images1.length; i++) {
+
+        
+
+         if (!processImage && !downloadButton) { 
+            setProcessImage((prev) => {  // for the first click sets the button to processing
+                return true
+            });
+            
+         }
+         else if (processImage && downloadButton) {
+
+            
+            setIsUploading((prev) => { 
+                return false
+            });
+            setInputArray((prev) => { 
+                return []
+            });
+            
+            setProcessImage((prev) => { 
+                return false
+            });
+
+            const promise = new Promise((resolve) => {
+                if (checked1 && checked2) {
+                  for (let i = 0; i < images1.length; i++) {
+                    combineImages(images1[i], mockup14Pro, `mockup14Pro-${i}`, 80, 80);
+                    combineImages(images2[i], mockup14, `mockup14-${i}`, 348, 320);
+                  }
+                } else if (checked1) {
+                  for (let i = 0; i < images1.length; i++) {
+                    combineImages(images1[i], mockup14Pro, `mockup14Pro-${i}`, 80, 80);
+                  }
+                } else {
+                  for (let i = 0; i < images2.length; i++) {
+                    combineImages(images2[i], mockup14, `mockup14-${i}`, 348, 320);
+                  }
+                }
+                
+                resolve(); 
+              });
+            
+              promise.then(() => {
+                setDownloadButton((prev) => false);
+                setImages1((prev) => []);
+                setImages2((prev) => []);
+              });
       
-          combineImages(images1[i], mockup14Pro, `mockup14Pro-${i}`, 80, 80);
-         
-          
+         }
+        
         }
-      }
-      else { 
-        for (let i = 0; i < images1.length; i++) {
-      
-         
-          combineImages(images2[i], mockup14, `mockup14-${i}`, 348, 320);
-          
-        }
-      }
+    };
 
-      setTimeout(() => {
-        // Set downloadButton back to false after a delay
-        setDownloadButton(false);
-      }, 2000);
-      
-    }
-
-    
-    
-    
-
-  
-      
-  };
  
+
+useEffect(() => {
+    if (processImage) {
+      
+      const promises = [];
+  
+      for (let i = 0; i < inputArray.length; i++) {
+        const file = inputArray[i];
+        const reader = new FileReader();
+  
+        
+        const promise = new Promise((resolve) => {
+          reader.onload = (event) => {
+            const img = new Image();
+            img.src = event.target.result;
+            img.onload = () => {
+              
+              resizeImage(img, setImages1, 1180, 2556);
+             
+              resizeImage(img, setImages2, 2339, 5063);
+  
+              resolve(); 
+            };
+          };
+  
+          reader.readAsDataURL(file);
+        });
+  
+        promises.push(promise);
+      }
+  
+     
+      Promise.all(promises).then(() => {
+        setDownloadButton(true);
+      });
+    }
+  }, [processImage]);
+  
+
+
+  let buttonText;
+  if (!processImage && !downloadButton) {
+    buttonText = 'Process Image';
+  } if( processImage && !downloadButton) { 
+    buttonText = 'Processing'
+  }  if (processImage && downloadButton) {
+    buttonText = 'Download Images';
+  }  if (!processImage && downloadButton) {
+    buttonText = 'Downloading';
+  }
+ 
+  useEffect(()=>{
+    console.log(buttonText)
+  }, [buttonText])
   
 
   return (
@@ -244,7 +349,7 @@ function ImageCombiner() {
       <>
         <CloudUploadIcon /> Upload Images
       </>)}
-       </InputLabel> <input id='image-upload' className='input-field' type="file" multiple accept="image/*" onChange={handleImageChange} hidden   /> 
+       </InputLabel> <input ref={fileInputRef} id='image-upload' className='input-field' type="file" multiple accept="image/*" onChange={handleImageChange} hidden disabled={( processImage && !downloadButton) || (!processImage && downloadButton) }  /> 
         </form>
         <Button onClick = {() => {setInputArray([]); setImages1([]); setImages2([]); setIsUploading(false) }} style={{margin:20, backgroundColor: 'blue', color: 'white'}}> Clear </Button>
     </div>
@@ -258,14 +363,16 @@ function ImageCombiner() {
         <FormControlLabel className='labels' label="iPhone 14"
         control={ <Checkbox checked={checked2} onChange={() => { setChecked2((prevChecked) => !prevChecked);}} />} />
     </div>
-        <Button className='buttonContainer'  onClick={handleClick} variant="contained" color='success'  style={{position: 'absolute',bottom: 120,right: 40 }} disabled={downloadButton} > {downloadButton ? 'Downloading' : 'Download Images'} </Button>
+        <Button className='buttonContainer'  onClick={handleClick} variant="contained" color='success'  style={{position: 'absolute',bottom: 120,right: 40 }} disabled={( processImage && !downloadButton) || (!processImage && downloadButton) } > 
+          {buttonText}
+            </Button>
     </div>
 </div>
     
   );
 }
 
-export default ImageCombiner;
+export default NewIphonesImageCombiner;
 
 
 
--- src/OldIphones.js
+++ src/OldIphones.js
@@ -1,5 +1,5 @@
 
-import React, { useState,useEffect} from 'react';
+import React, { useState,useEffect, useRef} from 'react';
 
 import iphoneSeMockup from './images/iphoneSeMockup/mockupSe.png';
 
@@ -10,13 +10,15 @@ import { InputLabel } from '@material-ui/core';
 import {Button,Checkbox, FormControlLabel } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-function OldIphonesPage() {
+function OldIphonesImageCombiner() {
     const navigate = useNavigate();
-  const [checked1, setChecked1] = useState(true);
+    const fileInputRef = useRef(null);
 
+  const [checked1, setChecked1] = useState(true);
   const [inputArray,setInputArray] = useState([]);  
   const [downloadButton, setDownloadButton] = useState(false)
   const [isUploading, setIsUploading] = useState(false);
+  const [processImage, setProcessImage] = useState(false)
 
 
   const [images1, setImages1] = useState([]); //for mockup stores resized images
@@ -125,8 +127,22 @@ function OldIphonesPage() {
 
 
   const handleImageChange = (e) => {
-    setIsUploading(true);
-    setImages1([])
+    setInputArray((prev) => { 
+        return []
+    });
+
+    setImages1((prev) => { 
+        return []
+    });
+
+
+    setProcessImage((prev) => { 
+        return false
+    });
+
+    setDownloadButton((prev) => { 
+        return false
+    });
    
     const selectedFiles = e.target.files; // Get the list of selected files
     console.log("selectedFiles")
@@ -134,72 +150,134 @@ function OldIphonesPage() {
     console.log(selectedFiles.length)
 
     const fileArray = Array.from(selectedFiles);
-    setInputArray(fileArray);
+    if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+
+    setInputArray((prev) => { 
+        return fileArray
+    });
+
+    
+
+    setIsUploading((prev) => { 
+        return true
+    });
 
    };
  
-  useEffect (() => { 
-    console.log('printing inputArray in useEffect Hook for inputArray : ', inputArray)
-   
-    for(let i = 0 ; i < inputArray.length ; i++){ 
-      const file = inputArray[i];
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const img = new Image();
-      img.src = event.target.result;
-      img.onload = () => {
-        // Resize image1
-        resizeImage(img,setImages1, 756, 1334);
-        
-        
-      };
-    };
-
-    reader.readAsDataURL(file);
-
-    }
-
-  },[inputArray])
-
-
-
  
-
-
-  const handleClick = () => {
+ const handleClick = () => {
+    
     if(inputArray.length === 0){ 
         alert('Please select a few items first')
         return
     }
-    setIsUploading(false);
+
+    
 
     if (!checked1){ 
       alert("Please check the boxes for iphone");
     } else { 
 
-        setDownloadButton(true)
+        
 
-      if (checked1) { 
-        for (let i = 0; i < images1.length; i++) {
+        
+
+         if (!processImage && !downloadButton) { 
+            setProcessImage((prev) => {  // for the first click sets the button to processing
+                return true
+            });
+            
+         }
+         else if (processImage && downloadButton) {
+
+            
+            setIsUploading((prev) => { 
+                return false
+            });
+            setInputArray((prev) => { 
+                return []
+            });
+            
+            setProcessImage((prev) => { 
+                return false
+            });
+
+            const promise = new Promise((resolve) => {
+                if (checked1) {
+                  for (let i = 0; i < images1.length; i++) {
+                    combineImages(images1[i], mockupSe, `mockup14Se-${i}`, 150, 300);
+                    
+                  }
+                } 
+                
+                resolve(); 
+              });
+            
+              promise.then(() => {
+                setDownloadButton((prev) => false);
+                setImages1((prev) => []);
+                
+              });
       
-          combineImages(images1[i], mockupSe, `mockup14Se-${i}`, 150, 300);
-          
-          
+         }
+        
         }
+    };
 
-      } 
-     
-
-      setTimeout(() => {
-        // Set downloadButton back to false after a delay
-        setDownloadButton(false);
-      }, 2000);
+    useEffect(() => {
+        if (processImage) {
+          
+          const promises = [];
       
-    }
-
+          for (let i = 0; i < inputArray.length; i++) {
+            const file = inputArray[i];
+            const reader = new FileReader();
+      
+            
+            const promise = new Promise((resolve) => {
+              reader.onload = (event) => {
+                const img = new Image();
+                img.src = event.target.result;
+                img.onload = () => {
+                  
+                    resizeImage(img,setImages1, 756, 1334);
+      
+                  resolve(); 
+                };
+              };
+      
+              reader.readAsDataURL(file);
+            });
+      
+            promises.push(promise);
+          }
+      
+         
+          Promise.all(promises).then(() => {
+            setDownloadButton(true);
+          });
+        }
+      }, [processImage]);
+      
     
- };
+    
+      let buttonText;
+      if (!processImage && !downloadButton) {
+        buttonText = 'Process Image';
+      } if( processImage && !downloadButton) { 
+        buttonText = 'Processing'
+      }  if (processImage && downloadButton) {
+        buttonText = 'Download Images';
+      }  if (!processImage && downloadButton) {
+        buttonText = 'Downloading';
+      }
+     
+      useEffect(()=>{
+        console.log(buttonText)
+      }, [buttonText])
+      
  
   
 
@@ -218,7 +296,7 @@ function OldIphonesPage() {
       <>
         <CloudUploadIcon /> Upload Images
       </>)}
-       </InputLabel> <input id='image-upload' className='input-field' type="file" multiple accept="image/*" onChange={handleImageChange} hidden   /> 
+       </InputLabel> <input ref={fileInputRef} id='image-upload' className='input-field' type="file" multiple accept="image/*" onChange={handleImageChange} hidden disabled={( processImage && !downloadButton) || (!processImage && downloadButton) }  /> 
         </form>
         <Button onClick = {() => {setInputArray([]); setImages1([]); setIsUploading(false) }} style={{margin:20, backgroundColor: 'blue', color: 'white'}}> Clear </Button>
     </div>
@@ -227,14 +305,14 @@ function OldIphonesPage() {
         control={<Checkbox checked={checked1} onChange={() => {  setChecked1((prevChecked) => !prevChecked);}} /> }/>
        
     </div>
-        <Button className='buttonContainer'  onClick={handleClick} variant="contained" color='success'  style={{position: 'absolute',bottom: 120,right: 40 }} disabled={downloadButton} > {downloadButton ? 'Downloading' : 'Download Images'} </Button>
+        <Button className='buttonContainer'  onClick={handleClick} variant="contained" color='success'  style={{position: 'absolute',bottom: 120,right: 40 }} disabled={( processImage && !downloadButton) || (!processImage && downloadButton) } >  {buttonText} </Button>
     </div>
 </div>
     
   );
 }
 
-export default OldIphonesPage;
+export default OldIphonesImageCombiner;
 
 
 
